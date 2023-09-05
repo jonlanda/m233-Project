23d0ebb84b4ce8e642865283960aa672fa186664
Jon Landa <jonlanda06@gmail.com>
2023-09-05 11:34:34 +0200

    Made ok tests for Materials

--- src/test/java/ch/zli/m223/coworkingspace/MaterialResourceTest.java
+++ src/test/java/ch/zli/m223/coworkingspace/MaterialResourceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import ch.zli.m223.coworkingspace.model.ApplicationUser;
+import ch.zli.m223.coworkingspace.model.Material;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
@@ -15,7 +16,8 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class UserResourceTest {
+public class MaterialResourceTest {
+
     private String token;
 
     private ApplicationUser appUser;
@@ -54,119 +56,103 @@ public class UserResourceTest {
     public void testIndexEndpoint() {
         given()
                 .auth().oauth2(token)
-                .when().get("/users")
+                .when().get("/materials")
                 .then()
                 .statusCode(200)
-                .body(is(
-                        "[{\"id\":15,\"firstname\":\"Jon\",\"lastname\":\"Landa\",\"password\":\"123\",\"isAdmin\":true,\"email\":\"jon@test.ch\",\"booking\":[]}]"));
+                .body(is("[]"));
     }
 
     @Test
-    public void testSpecificUserEndpoint() {
-        ApplicationUser user = new ApplicationUser();
-        user.setFirstname("Jon");
-        user.setLastname("Landa2");
-        user.setPassword("bla");
-        user.setIsAdmin(false);
-        user.setEmail("jon@jon.jon");
-        user = given().contentType(ContentType.JSON)
-                .body(user)
+    public void testSpecificMaterialEndpoint() {
+        Material material = new Material();
+        material.setTitle("Keyboard");
+        material.setAmount(5);
+        material = given().contentType(ContentType.JSON)
+                .body(material)
                 .auth().oauth2(token)
-                .when().post("/users")
+                .when().post("/materials")
                 .then()
                 .statusCode(200)
-                .extract().body().as(ApplicationUser.class);
+                .extract().body().as(Material.class);
         given().contentType(ContentType.JSON)
                 .auth().oauth2(token)
-                .when().get("/users/" + user.getId())
+                .when().get("/materials/" + material.getId())
                 .then()
                 .statusCode(200)
-                .body("firstname", is(user.getFirstname().toString()))
-                .body("email", is(user.getEmail().toString()))
-                .body("password", is(user.getPassword().toString()));
+                .body("title", is(material.getTitle().toString()))
+                .body("amount", is(material.getAmount()));
         given().contentType(ContentType.JSON)
                 .auth().oauth2(token)
-                .when().delete("/users/" + user.getId())
+                .when().delete("/materials/" + material.getId())
                 .then()
                 .statusCode(204);
-
     }
 
     @Test
     public void testCreateEndpoint() {
-        ApplicationUser user = new ApplicationUser();
-        user.setFirstname("Jon");
-        user.setLastname("Landa2");
-        user.setPassword("bla");
-        user.setIsAdmin(false);
-        user.setEmail("jon@jon.jon");
-        user = given().contentType(ContentType.JSON)
-                .body(user)
+        Material material = new Material();
+        material.setTitle("Keyboard");
+        material.setAmount(5);
+        material = given().contentType(ContentType.JSON)
+                .body(material)
                 .auth().oauth2(token)
-                .when().post("/users")
+                .when().post("/materials")
                 .then()
                 .statusCode(200)
-                .body("firstname", is(user.getFirstname().toString()))
-                .body("email", is(user.getEmail().toString()))
-                .body("password", is(user.getPassword().toString()))
-                .extract().body().as(ApplicationUser.class);
+                .body("title", is(material.getTitle().toString()))
+                .body("amount", is(material.getAmount()))
+                .extract().body().as(Material.class);
         given().contentType(ContentType.JSON)
                 .auth().oauth2(token)
-                .when().delete("/users/" + user.getId())
+                .when().delete("/materials/" + material.getId())
                 .then()
                 .statusCode(204);
     }
 
     @Test
     public void testDeleteEndpoint() {
-        ApplicationUser user = new ApplicationUser();
-        user.setFirstname("Jon");
-        user.setLastname("Landa2");
-        user.setPassword("bla");
-        user.setIsAdmin(false);
-        user.setEmail("jon@jon.jon");
-        user = given().contentType(ContentType.JSON)
-                .body(user)
+        Material material = new Material();
+        material.setTitle("Keyboard");
+        material.setAmount(5);
+        material = given().contentType(ContentType.JSON)
+                .body(material)
                 .auth().oauth2(token)
-                .when().post("/users")
+                .when().post("/materials")
                 .then()
                 .statusCode(200)
-                .extract().body().as(ApplicationUser.class);
+                .extract().body().as(Material.class);
         given()
                 .auth().oauth2(token)
-                .when().delete("/users/" + user.getId())
+                .when().delete("/materials/" + material.getId())
                 .then()
                 .statusCode(204);
     }
 
     @Test
     public void testEditEndpoint() {
-        ApplicationUser user = new ApplicationUser();
-        user.setFirstname("Jon");
-        user.setLastname("Landa2");
-        user.setPassword("bla");
-        user.setIsAdmin(false);
-        user.setEmail("jon@jon.jon");
-        user = given().contentType(ContentType.JSON)
-                .body(user)
+        Material material = new Material();
+        material.setTitle("Keyboard");
+        material.setAmount(5);
+        material = given().contentType(ContentType.JSON)
+                .body(material)
                 .auth().oauth2(token)
-                .when().post("/users")
+                .when().post("/materials")
                 .then()
                 .statusCode(200)
-                .extract().body().as(ApplicationUser.class);
-        user.setFirstname("Jono");
-        user.setPassword("bla123");
+                .extract().body().as(Material.class);
+        material.setTitle("Mouse");
+        material.setAmount(7);
         given().contentType(ContentType.JSON)
-                .body(user)
+                .body(material)
                 .auth().oauth2(token)
-                .when().put("/users")
+                .when().put("/materials")
                 .then()
                 .statusCode(200)
-                .body("firstname", is(user.getFirstname().toString()))
-                .body("password", is(user.getPassword().toString()));
+                .body("title", is(material.getTitle().toString()))
+                .body("amount", is(material.getAmount()));
         given()
                 .auth().oauth2(token)
-                .when().delete("/users/" + user.getId())
+                .when().delete("/materials/" + material.getId())
                 .then()
                 .statusCode(204);
     }
